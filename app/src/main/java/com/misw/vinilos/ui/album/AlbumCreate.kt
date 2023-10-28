f3386555e44e4a8e6d009c1a651a81df881249ce
emrz16 <e.rodriguezz@uniandes.edu.co>
2023-10-27 21:39:26 -0600

    mejora de ui y date

--- app/src/main/java/com/misw/vinilos/ui/album/AlbumCreate.kt
+++ app/src/main/java/com/misw/vinilos/ui/album/AlbumCreate.kt
@@ -2,6 +2,7 @@ package com.misw.vinilos.ui.album
 
 import android.app.DatePickerDialog
 import android.widget.DatePicker
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -32,6 +36,9 @@ import androidx.compose.ui.unit.sp
 import java.time.Instant
 import java.time.ZoneOffset
 import java.util.Calendar
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.painter.ColorPainter
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,6 +46,10 @@ fun AlbumCreate() {
     var imagePath by remember { mutableStateOf("") }
     var albumName by remember { mutableStateOf("") }
     var generoName by remember { mutableStateOf("") }
+    var canciones  = remember {
+        mutableStateListOf("")
+    }
+    val defaultImage = ColorPainter(color = Color.Gray)
     val context = LocalContext.current
     var selectedDateText by remember { mutableStateOf("") }
     val calendar = Calendar.getInstance()
@@ -51,67 +62,115 @@ fun AlbumCreate() {
             selectedDateText = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
         }, year, month, dayOfMonth
     )
-    Card(modifier = Modifier.background(Color.Transparent)) {
-        Text(
-            text = "Crear Album",
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primary,
-            fontSize = 24.sp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp, start = 10.dp, end = 10.dp, top = 10.dp)
-        )
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            TextField(
-                value = imagePath,
-                onValueChange = { },
-                label = { Text("Path de la Imagen") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 16.dp)
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        item {
+            Text(
+                text = "Crear Álbum",
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(bottom = 8.dp)
             )
         }
+
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = defaultImage,
+                    contentDescription = "Imagen por defecto",
+                    modifier = Modifier.size(20.dp)
+                )
+                TextField(
+                    value = imagePath,
+                    onValueChange = { imagePath = it },
+                    label = { Text("Path de la Imagen") },
+                    modifier = Modifier.fillMaxWidth().padding(start = 16.dp)
+                )
+            }
+        }
+
+
+    item {
         OutlinedTextField(
             value = albumName,
-            onValueChange = { },
+            onValueChange = { albumName = it },
             label = { Text("Nombre del Álbum") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
         )
+    }
+
+    item {
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
                 text = if (selectedDateText.isNotEmpty()) {
-                    "Selected date is $selectedDateText"
+                    "$selectedDateText"
                 } else {
-                    "Please pick a date"
+                    "Por favor, selecciona una fecha"
                 }
             )
 
             Button(
+                modifier = Modifier.size(30.dp),
                 onClick = {
                     datePicker.show()
                 }
             ) {
-                Text(text = "Select a date")
+                Text(text = "Fecha de lanzamiento")
             }
         }
+    }
+
+    item {
         OutlinedTextField(
             value = generoName,
-            onValueChange = { },
+            onValueChange = { generoName = it },
             label = { Text("Género") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
         )
     }
+    items(canciones.size) { index ->
+        OutlinedTextField(
+            value = canciones[index],
+            onValueChange = { canciones[index] = it },
+            label = { Text("Canción") },
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
+        )
+    }
+
+
+item {
+    Button(
+        onClick = {
+            canciones.add("")
+        },
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text("+")
+    }
 }
+
+item {
+    Button(
+        onClick = {
+            // Lógica para crear el álbum
+        },
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text("Crear")
+    }
+}
+
+    }}
 
 @Preview
 @Composable
